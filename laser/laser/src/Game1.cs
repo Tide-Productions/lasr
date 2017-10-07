963d9a02adb0c6c605dbc1e99693f4ab38a29218
Michael Mario Kubicki <Kubicki-Michael@live.de>
2017-10-07 13:21:17 +0200

    init
    Signed-off-by: Michael Mario Kubicki <Kubicki-Michael@live.de>

---
+++ laser/laser/src/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace laser
+{
+    public class Game1 : Core
+    {
+        public Game1() : base(width: 1280, height: 720, isFullScreen: false, enableEntitySystems: true, contentDirectory: "Content", windowTitle: "LAS??R")
+        { }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
